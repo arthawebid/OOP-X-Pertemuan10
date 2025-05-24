152948b1beac525c1cf6163525127b1799e8ffdf
OOP-X <arthawebid@gmail.com>
2025-05-24 17:14:45 +0800

    pertemuan 10

---
+++ src/pertemuan10/Pertemuan10.java
@@ -0,0 +1,15 @@
+package Pertemuan10;
+/**
+ *
+ * @author INSTIKI
+ */
+public class Pertemuan10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
